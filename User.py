8e011480ae994d527d52d9e8e28008047f3824a0
DualNixSix <H4L0R34CHFTW@HOTMAIL.COM>
2025-11-15 09:35:35 -0700

    Completed User.py assignment.

--- User.py
+++ User.py
@@ -1,1 +1,11 @@
-# your User class goes here
+class user:
+    def __init__(self, name, email, dl):
+        self.name = name
+        self.email = email 
+        self.dl = dl 
+    def __str__(self):
+        return f"My Name is {self.name}"
+
+
+Alice = user("Alice", "Alice@mail.com", 12345)
+Bob = user("Bob", "Bob@mail.com", 54321)
